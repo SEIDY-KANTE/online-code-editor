ec13022fc30548c9e74524b99aa222ee87249422
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-06-07 15:10:19 +0300

    HTML and some CSS style implementation

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+let htmCode = document.querySelector("#html-editor");
+let cssCode = document.querySelector("#css-editor");
+let jsCode = document.querySelector("#js-editor");
+
+let output = document.querySelector("#output");
